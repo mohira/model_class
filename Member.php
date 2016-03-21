dcd3c02fdfaa36c5cdfdd82c0fb9c8fea82b1766
mohira <m.ohira@nowall.co.jp>
2016-03-21 10:48:59 +0900

    update()実装

--- Member.php
+++ Member.php
@@ -87,6 +87,73 @@ class Member {
         return false;
     }
 
+    public function update($params) {
+
+        // var_dump($params);
+
+        // $params = array(
+        //     'id' => 17,
+        //     'name' => 'EDIT_NAME_2',
+        // );
+
+        // echo '<hr>';
+
+        $oldRecord = $this->findOneById($params['id']);
+
+        // $oldRecord = array(
+        //     'id' => 17,
+        //     'name' => 'kashiwagi',  // ← ココを編集した
+        //     'email' => 'kashiwagi@example',
+        //     'created_at' => '2016-03-21 09:44:08',
+        //     'updated_at' => '2016-03-21 09:44:08';
+        //     )
+
+        // var_dump($oldRecord);
+
+        // $final_params = array(
+        //     'id' => 17,
+        //     'name' => 'EDIT_NAME_2',  // ← ココが編集したいやつ
+        //     'email' => 'kashiwagi@example',
+        //     'created_at' => '2016-03-21 09:44:08',
+        //     'updated_at' => '2016-03-21 09:44:08';
+        //     )
+
+        if (!$oldRecord) {
+            // return false;
+            return 'NotFound';
+        }
+
+        $params = array_merge($oldRecord, $params);
+        // $params = array_merge($params, $oldRecord);
+        // var_dump($params);
+        // exit;
+
+        // ここからはレコードが存在するときの処理 => 更新処理をする
+        $sql = "update members set name = :name, email = :email,
+                updated_at = :updated_at where id = :id";
+
+        $stmt = $this->dbh->prepare($sql);
+
+        $stmt->bindParam(":id"        , $params['id']);
+        $stmt->bindParam(":name"      , $params['name']);
+        $stmt->bindParam(":email"     , $params['email']);
+        $stmt->bindParam(":updated_at", date('Y-m-d H:i:s'));
+
+        return $stmt->execute();
+
+    }
+
+// 編集用データ
+// $edit_data = array(
+//     'id'    => 18, // 必ず渡される
+//     'name'  => 'EDIT_NAME',
+// );
+
+
+// // // 編集処理
+// $member->update($edit_data);
+
+
 
 
 }
@@ -127,14 +194,31 @@ $record_1 = $member->findOneById(4);
 // var_dump($record_not_find);
 
 
-if ($member->delete(5)) {
-    echo "削除できた！！";
+// if ($member->delete(5)) {
+//     echo "削除できた！！";
+// } else {
+//     echo 'そんなデータはありませんでした';
+// }
+
+// 編集用データ
+$edit_data = array(
+    'id' => 168888888888888888888888888888,
+    'name' => 'EDIT_NAME_2222222222222',
+    // 'email' => 'EDIT_NAME@example.com',
+);
+
+
+// 編集処理
+$result = $member->update($edit_data);
+if ($result === 'NotFound') {
+    echo 'レコードが見つかりませんでした';
+} elseif ($result === true) {
+    echo '編集に成功しました！';
 } else {
-    echo 'そんなデータはありませんでした';
+    echo '編集できませんでした。。。';
 }
 
-
-
+var_dump($member->update($edit_data));
 
 
 
